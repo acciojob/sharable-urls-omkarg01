4b90e730e7f1dbe71de329a1d6200afccf5a9fab
Omkar Gujja <67428719+omkarg01@users.noreply.github.com>
2025-08-09 20:16:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+
+
+document.getElementById("button").addEventListener('click', function(e) {
+	e.preventDefault();
+
+	const form = document.querySelector("form");
+	const formdata = new FormData(form)
+	const name = formdata.get('name');
+	const year = formdata.get('year');
+
+	let url = `https://localhost:8080/?name=${name}&year=${year}`;
+
+	document.querySelector('h3').innerText = url
+	
+})
